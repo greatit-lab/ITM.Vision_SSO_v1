5fd78cd56d7f6e7f453b7ae7aed0c0acb154cc3d
greatit-lab <gil2zum@gmail.com>
2025-12-01 10:54:42 +0900

    Add missing import statement in equipment.module.ts

--- backend/src/equipment/equipment.module.ts
+++ backend/src/equipment/equipment.module.ts
@@ -1,3 +1,4 @@
+// src/equipment/equipment.module.ts
 import { Module } from '@nestjs/common';
 import { EquipmentController } from './equipment.controller';
 import { EquipmentService } from './equipment.service';
