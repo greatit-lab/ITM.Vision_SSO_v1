d9b5ae035795da1605379edd13ef5a85659dcc57
greatit-lab <gil2zum@gmail.com>
2025-12-11 13:39:35 +0900

    Create health.module.ts -- 1211_13

---
+++ backend/src/health/health.module.ts
@@ -0,0 +1,11 @@
+// backend/src/health/health.module.ts
+import { Module } from '@nestjs/common';
+import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+import { PrismaService } from '../prisma.service';
+
+@Module({
+  controllers: [HealthController],
+  providers: [HealthService, PrismaService],
+})
+export class HealthModule {}
