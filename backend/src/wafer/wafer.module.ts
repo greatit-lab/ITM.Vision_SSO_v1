16b706036a7045feb9015ab0d9f27d36c58d35f5
greatit-lab <gil2zum@gmail.com>
2026-01-07 14:50:29 +0900

    Update wafer.module.ts --0107_14

--- backend/src/wafer/wafer.module.ts
+++ backend/src/wafer/wafer.module.ts
@@ -1,11 +1,17 @@
-// itm-data-api/src/wafer/wafer.module.ts
+// backend/src/wafer/wafer.module.ts
 import { Module } from '@nestjs/common';
-import { WaferController } from './wafer.controller';
+import { HttpModule } from '@nestjs/axios';
 import { WaferService } from './wafer.service';
-import { PrismaService } from '../prisma.service';
+import { WaferController } from './wafer.controller';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 30000, // 대용량 데이터 조회를 위해 타임아웃 30초로 넉넉하게 설정
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [WaferController],
-  providers: [WaferService, PrismaService],
+  providers: [WaferService],
 })
 export class WaferModule {}
