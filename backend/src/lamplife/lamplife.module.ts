37834531f3e55aaa1a242294b407e27baac4af1a
greatit-lab <gil2zum@gmail.com>
2026-01-09 09:05:52 +0900

    Update lamplife.module.ts --0109_09

--- backend/src/lamplife/lamplife.module.ts
+++ backend/src/lamplife/lamplife.module.ts
@@ -1,11 +1,17 @@
 // backend/src/lamplife/lamplife.module.ts
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { LampLifeController } from './lamplife.controller';
 import { LampLifeService } from './lamplife.service';
-import { PrismaService } from '../prisma.service';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 30000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [LampLifeController],
-  providers: [LampLifeService, PrismaService],
+  providers: [LampLifeService],
 })
 export class LampLifeModule {}
