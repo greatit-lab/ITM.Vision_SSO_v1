1980dcb779ae26f355948fa9f57c2143d201a1ad
greatit-lab <gil2zum@gmail.com>
2026-01-09 09:38:16 +0900

    Update menu.module.ts --0109_09

--- backend/src/menu/menu.module.ts
+++ backend/src/menu/menu.module.ts
@@ -1,11 +1,17 @@
 // backend/src/menu/menu.module.ts
 import { Module } from '@nestjs/common';
-import { MenuService } from './menu.service';
+import { HttpModule } from '@nestjs/axios';
 import { MenuController } from './menu.controller';
-import { PrismaService } from '../prisma.service';
+import { MenuService } from './menu.service';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 30000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [MenuController],
-  providers: [MenuService, PrismaService],
+  providers: [MenuService],
 })
 export class MenuModule {}
