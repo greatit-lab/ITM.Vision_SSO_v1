148a1f590ebbea777eb177549417fc77db14fe46
greatit-lab <gil2zum@gmail.com>
2026-01-13 08:39:49 +0900

    Update infra.controller.ts --0113_08

--- backend/src/infra/infra.controller.ts
+++ backend/src/infra/infra.controller.ts
@@ -8,8 +8,13 @@ import {
   Body,
   Param,
 } from '@nestjs/common';
-import { InfraService } from './infra.service';
-import { Prisma } from '@prisma/client';
+import {
+  InfraService,
+  CreateSdwtDto,
+  UpdateSdwtDto,
+  CreateCfgServerDto,
+  UpdateCfgServerDto,
+} from './infra.service';
 
 @Controller('infra')
 export class InfraController {
@@ -21,17 +26,17 @@ export class InfraController {
     return await this.infraService.getSdwts();
   }
 
-  // [추가] SDWT 생성
+  // [추가] SDWT 생성 [변경: 타입 수정]
   @Post('sdwt')
-  async createSdwt(@Body() body: Prisma.RefSdwtCreateInput) {
+  async createSdwt(@Body() body: CreateSdwtDto) {
     return await this.infraService.createSdwt(body);
   }
 
-  // [추가] SDWT 수정
+  // [추가] SDWT 수정 [변경: 타입 수정]
   @Put('sdwt/:id')
   async updateSdwt(
     @Param('id') id: string,
-    @Body() body: Prisma.RefSdwtUpdateInput,
+    @Body() body: UpdateSdwtDto,
   ) {
     return await this.infraService.updateSdwt(id, body);
   }
@@ -48,15 +53,17 @@ export class InfraController {
     return await this.infraService.getAgentServers();
   }
 
+  // [변경: 타입 수정]
   @Post('agent-server')
-  async createAgentServer(@Body() body: Prisma.CfgServerCreateInput) {
+  async createAgentServer(@Body() body: CreateCfgServerDto) {
     return await this.infraService.createAgentServer(body);
   }
 
+  // [변경: 타입 수정]
   @Put('agent-server/:id')
   async updateAgentServer(
     @Param('id') id: string,
-    @Body() body: Prisma.CfgServerUpdateInput,
+    @Body() body: UpdateCfgServerDto,
   ) {
     return await this.infraService.updateAgentServer(id, body);
   }
