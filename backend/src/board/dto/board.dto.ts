77fdb8e94d1d8104f7cc4c48d27b56ecd97c67b8
greatit-lab <gil2zum@gmail.com>
2026-01-21 15:58:01 +0900

    Create board.dto.ts --0121_15

---
+++ backend/src/board/dto/board.dto.ts
@@ -0,0 +1,40 @@
+// backend/src/board/dto/board.dto.ts
+import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsString()
+  @IsNotEmpty()
+  authorId: string;
+
+  @IsString()
+  @IsOptional()
+  category?: string;
+
+  @IsString()
+  @IsOptional()
+  isSecret?: string;
+}
+
+export class CreateCommentDto {
+  @IsNotEmpty()
+  postId: number;
+
+  @IsString()
+  @IsNotEmpty()
+  authorId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsOptional()
+  parentId?: number;
+}
