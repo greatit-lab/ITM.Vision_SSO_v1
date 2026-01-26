06a909cdb2ca5d54d163e78239d9681afe4096df
greatit-lab <gil2zum@gmail.com>
2026-01-26 17:02:36 +0900

    Update manual.module.ts --0126_17-1

--- backend/src/manual/manual.module.ts
+++ backend/src/manual/manual.module.ts
@@ -1,4 +1,4 @@
-// ITM-Data-API/src/manual/manual.module.ts
+// backend/src/manual/manual.module.ts
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios'; // [중요] HTTP 통신 모듈
 import { ManualController } from './manual.controller';
