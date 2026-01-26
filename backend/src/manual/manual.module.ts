2188e53f02a39f6c59e99792a3269053bce89d00
greatit-lab <gil2zum@gmail.com>
2026-01-26 17:02:00 +0900

    Update manual.module.ts --0126_17

--- backend/src/manual/manual.module.ts
+++ backend/src/manual/manual.module.ts
@@ -1,11 +1,12 @@
 // ITM-Data-API/src/manual/manual.module.ts
 import { Module } from '@nestjs/common';
-import { ManualService } from './manual.service';
+import { HttpModule } from '@nestjs/axios'; // [중요] HTTP 통신 모듈
 import { ManualController } from './manual.controller';
-import { PrismaService } from '../prisma.service';
+import { ManualService } from './manual.service';
 
 @Module({
+  imports: [HttpModule], // HttpService 사용을 위해 import
   controllers: [ManualController],
-  providers: [ManualService, PrismaService],
+  providers: [ManualService],
 })
 export class ManualModule {}
