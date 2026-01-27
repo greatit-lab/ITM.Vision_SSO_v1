9aa65432f78bda29382c2df7d2570815d3d39b4a
greatit-lab <gil2zum@gmail.com>
2026-01-27 15:41:11 +0900

    Update alert.service.ts --0127_15-1

--- backend/src/alert/alert.service.ts
+++ backend/src/alert/alert.service.ts
@@ -4,21 +4,42 @@ import { DataApiService } from '../common/data-api.service';
 
 @Injectable()
 export class AlertService {
+  // Data API의 Controller 도메인
   private readonly DOMAIN = 'alert';
 
   constructor(private readonly dataApiService: DataApiService) {}
 
-  async getAlerts(userId?: string, token?: string) {
-    const params = userId ? { userId } : undefined;
-    
-    // [수정] 6번째 인자(options) 객체에 token 전달
+  // 1. 내 알림 조회 (Data API 호출)
+  async getMyAlerts(userId: string) {
+    // GET http://[DataAPI]/api/alert?userId=...
     return this.dataApiService.request<any[]>(
       this.DOMAIN,
       'get',
-      '',
+      '', // endpoint (루트)
       undefined,
-      params,
-      { token } // RequestOptions 객체로 전달
+      { userId } // Query Parameter
+    );
+  }
+
+  // 2. 안 읽은 개수 조회
+  async getUnreadCount(userId: string) {
+    // GET http://[DataAPI]/api/alert/unread-count?userId=...
+    return this.dataApiService.request<number>(
+      this.DOMAIN,
+      'get',
+      'unread-count',
+      undefined,
+      { userId }
+    );
+  }
+
+  // 3. 읽음 처리
+  async readAlert(id: number) {
+    // POST http://[DataAPI]/api/alert/:id/read
+    return this.dataApiService.request(
+      this.DOMAIN,
+      'post',
+      `${id}/read`
     );
   }
 }
