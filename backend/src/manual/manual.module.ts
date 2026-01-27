e9324ad5100038ad7eb017b40b996d15e3cf283f
greatit-lab <gil2zum@gmail.com>
2026-01-27 15:19:40 +0900

    Update manual.module.ts --0127_15

--- backend/src/manual/manual.module.ts
+++ backend/src/manual/manual.module.ts
@@ -1,11 +1,11 @@
 // backend/src/manual/manual.module.ts
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios'; // [중요] HTTP 통신 모듈
 import { ManualController } from './manual.controller';
 import { ManualService } from './manual.service';
+import { CommonModule } from '../common/common.module'; // [수정] CommonModule 추가
 
 @Module({
-  imports: [HttpModule], // HttpService 사용을 위해 import
+  imports: [CommonModule], // [수정] DataApiService 사용을 위해 CommonModule 등록
   controllers: [ManualController],
   providers: [ManualService],
 })
