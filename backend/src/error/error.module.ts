fb5c32b9e245de0a829111324572f47406129cb0
greatit-lab <gil2zum@gmail.com>
2026-01-28 15:25:01 +0900

    Update error.module.ts --0128_15-1

--- backend/src/error/error.module.ts
+++ backend/src/error/error.module.ts
@@ -1,15 +1,15 @@
 // backend/src/error/error.module.ts
 import { Module } from '@nestjs/common';
-import { CommonModule } from '../common/common.module'; // [핵심] DataApiService 사용을 위해 필요
+import { CommonModule } from '../common/common.module'; // [핵심] DataApiService 사용을 위해 임포트
 import { ErrorController } from './error.controller';
 import { ErrorService } from './error.service';
 
 @Module({
   imports: [
-    CommonModule, // [수정] HttpModule 대신 CommonModule 사용
+    CommonModule, // [수정] HttpModule 대신 CommonModule을 사용하여 DataApiService 의존성 해결
   ],
-  controllers: [ErrorController], // [중요] 컨트롤러 등록 확인
+  controllers: [ErrorController],
   providers: [ErrorService],
-  exports: [ErrorService],
+  exports: [ErrorService], // [추가] 다른 모듈에서 ErrorService를 사용할 수 있도록 내보내기
 })
 export class ErrorModule {}
