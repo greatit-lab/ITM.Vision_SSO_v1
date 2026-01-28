dd4a77ce857639587f86af2e6c90d938b6dace1c
greatit-lab <gil2zum@gmail.com>
2026-01-28 15:45:33 +0900

    Update health.controller.ts --0128_15

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -2,11 +2,11 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { HealthService } from './health.service';
 
-// [수정] 'api/Health' -> 'Health'
 @Controller('Health') 
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
 
+  // [수정] baseDate 파라미터 제거
   @Get('summary')
   getSummary(@Query('site') site: string, @Query('sdwt') sdwt?: string) {
     return this.healthService.getHealthSummary(site, sdwt);
