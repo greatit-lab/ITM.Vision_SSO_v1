f591eb8b0836d99fad4e0867b0e320cbc777c164
greatit-lab <gil2zum@gmail.com>
2026-06-29 11:13:43 +0900

    Fix naming of MailRecipient module and classes --0629_11

--- backend/src/mail-recipient/mail-recipient.module.ts
+++ backend/src/mail-recipient/mail-recipient.module.ts
@@ -1,11 +1,11 @@
 // backend/src/mail-recipient/mail-recipient.module.ts
 import { Module } from '@nestjs/common';
-import { MailREcipientController } from './mail-recipient.controller';
-import { MailREcipientService } from './mail-recipient.service';
+import { MailRecipientController } from './mail-recipient.controller';
+import { MailRecipientService } from './mail-recipient.service';
 
 @Module({
   controllers: [MailRecipientController],
   providers: [MailRecipientService],
   exports: [MailRecipientService],
 })
-export class MailRecipientModuleP {}
+export class MailRecipientModule {}
